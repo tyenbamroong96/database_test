4edb8519a065739a8c0496cfa77e9c0ff5c745ca
Thomas Yenbamroong <pthomas.y@gmail.com>
2018-02-09 13:55:02 +0000

    d

--- home.php
+++ home.php
@@ -56,8 +56,7 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
     <?php
     $logged_in = $_SESSION['logged_in'];
     // echo $logged_in;
-    if ($logged_in == 'TRUE') {
-        include 'header_member.php';
+    if ($logged_in == TRUE) {
     } else {
         include 'header.php';
     }
