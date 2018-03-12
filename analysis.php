927ec41dd08a07944a9429326413e570877af67d
Thomas Yenbamroong <pthomas.y@gmail.com>
2018-03-12 12:08:33 +0000

    c

--- analysis.php
+++ analysis.php
@@ -122,21 +122,37 @@ var brand = new Chart(ctx, {
     data: {
         labels: ["Rolex", "Casio", "Seiko"],
         datasets: [{
-            label: ["Rolex", "Casio", "Seiko"],
-            label: 'Casio',
-            data: [<?php echo $rolexcount; ?>, <?php echo $casiocount; ?>, <?php echo $seikocount ?>],
+            label: 'Rolex',
+            data: [<?php echo $rolexcount; ?>],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
             ],
             borderWidth: 1
-        }]
+
+        }, {
+            label: 'Casio',
+            data: [<?php echo $casiocount; ?>],
+            backgroundColor: [
+              'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+              'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+          } {
+            label: 'Seiko',
+            data: [<?php echo $seikocount; ?>],
+            backgroundColor: [
+              'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+          }],
     },
     options: {
         scales: {
