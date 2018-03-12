2d231c9d79ed7d577c91bf5eef013ae6d44b2fa8
Thomas Yenbamroong <pthomas.y@gmail.com>
2018-03-12 11:16:40 +0000

    commit

--- analysis.php
+++ analysis.php
@@ -157,12 +157,12 @@ var type = new Chart(ctx2, {
             label: 'Number of views',
             data: [<?php echo $analogcount; ?>, <?php echo $digitalcount; ?>],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)'
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)'
+                'rgba(75, 192, 192,1)',
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1
         }]
@@ -187,11 +187,11 @@ var condition = new Chart(ctx3, {
             label: 'Number of views',
             data: [<?php echo $newcount; ?>, <?php echo $usedcount; ?>],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
+                'rgba(255, 159, 64, 1)',
                 'rgba(54, 162, 235, 1)',
             ],
             borderWidth: 1
